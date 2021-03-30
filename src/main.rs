7750afc4567c4d611d68a90323bc7cf0c8d769e5
Masterchef365 <duncan.freeman1@gmail.com>
2021-03-29 17:03:55 -0700

    Nowww it's centered

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ impl App for MyApp {
     }
 
     fn next_frame(&mut self, engine: &mut dyn Engine) -> Result<FramePacket> {
-        let transform = Matrix4::new_scaling(1.);
+        let transform = Matrix4::new_scaling(0.001);
         //let transform = Matrix4::from_euler_angles(0.0, self.time, 0.0);
         let object = Object {
             material: self.material,
@@ -73,22 +73,22 @@ fn rainbow_cube() -> (Vec<Vertex>, Vec<u16>) {
 fn a_paper(depth: usize, color: [f32; 3]) -> (Vec<Vertex>, Vec<u16>) {
     let mut vertices = Vec::with_capacity(depth * 2);
     let mut horiz = 1.;
-    let mut vert = std::f32::consts::SQRT_2;
+    let mut vert = 1. / std::f32::consts::SQRT_2;
     let mut x = 0.;//-horiz / 3.;
     let mut y = 0.;//-vert / 3.;
     let mut add_vert = |x, y| vertices.push(Vertex::new([x, 0., y], color));
     let mut cycle = false;
     for _ in 0..depth {
         if cycle {
+            y += vert;
+            horiz *= -2.;
             add_vert(x + horiz, y);
             add_vert(x - horiz, y);
-            horiz /= -2.;
-            y += vert;
         } else {
+            x += horiz;
+            vert *= -2.;
             add_vert(x, y + vert);
             add_vert(x, y - vert);
-            vert /= -2.;
-            x += horiz;
         }
         cycle = !cycle;
     }
