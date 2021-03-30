54cf07454cd79f58aca0458800db87f2e99a64a0
Masterchef365 <duncan.freeman1@gmail.com>
2021-03-29 18:49:42 -0700

    Now it scales right

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use klystron::{
 };
 
 struct MyApp {
-    material: Material,
-    mesh: Mesh,
+    tri_material: Material,
+    cube_mesh: Mesh,
+    line_material: Material,
+    paper_mesh: Mesh,
     time: f32,
 }
 
@@ -16,30 +18,43 @@ impl App for MyApp {
     type Args = ();
 
     fn new(engine: &mut dyn Engine, _args: Self::Args) -> Result<Self> {
-        let material = engine.add_material(UNLIT_VERT, UNLIT_FRAG, DrawType::Lines)?;
-
+        let line_material = engine.add_material(UNLIT_VERT, UNLIT_FRAG, DrawType::Lines)?;
         let (vertices, indices) = a_paper(50, [0.0, 0.3, 1.0]);
-        let mesh = engine.add_mesh(&vertices, &indices)?;
+        let paper_mesh = engine.add_mesh(&vertices, &indices)?;
+
+        let tri_material = engine.add_material(UNLIT_VERT, UNLIT_FRAG, DrawType::Lines)?;
+        let (vertices, indices) = rainbow_cube();
+        let cube_mesh = engine.add_mesh(&vertices, &indices)?;
 
         Ok(Self {
-            mesh,
-            material,
+            paper_mesh,
+            line_material,
+            cube_mesh,
+            tri_material,
             time: 0.0,
         })
     }
 
     fn next_frame(&mut self, engine: &mut dyn Engine) -> Result<FramePacket> {
-        let transform = Matrix4::new_scaling(0.001);
-        //let transform = Matrix4::from_euler_angles(0.0, self.time, 0.0);
-        let object = Object {
-            material: self.material,
-            mesh: self.mesh,
+        let cube = Object {
+            material: self.tri_material,
+            mesh: self.cube_mesh,
+            transform: Matrix4::new_translation(&nalgebra::Vector3::new(0., 1., 0.)),
+        };
+
+        let repeat_scale = 2.0f32.powf(self.time * 2.);
+        let transform = Matrix4::new_scaling(repeat_scale * 0.0001);
+        let paper = Object {
+            material: self.line_material,
+            mesh: self.paper_mesh,
             transform,
         };
+
         engine.update_time_value(self.time)?;
-        self.time += 0.01;
+        self.time = (self.time + 0.001).fract();
+
         Ok(FramePacket {
-            objects: vec![object],
+            objects: vec![paper],
         })
     }
 }
@@ -72,12 +87,16 @@ fn rainbow_cube() -> (Vec<Vertex>, Vec<u16>) {
 
 fn a_paper(depth: usize, color: [f32; 3]) -> (Vec<Vertex>, Vec<u16>) {
     let mut vertices = Vec::with_capacity(depth * 2);
+    let mut add_vert = |x, y| vertices.push(Vertex::new([x, 0., y], color));
+
     let mut horiz = 1.;
     let mut vert = 1. / std::f32::consts::SQRT_2;
-    let mut x = 0.;//-horiz / 3.;
-    let mut y = 0.;//-vert / 3.;
-    let mut add_vert = |x, y| vertices.push(Vertex::new([x, 0., y], color));
+
+    let mut x = 0.;
+    let mut y = 0.;
+
     let mut cycle = false;
+
     for _ in 0..depth {
         if cycle {
             y += vert;
@@ -90,7 +109,9 @@ fn a_paper(depth: usize, color: [f32; 3]) -> (Vec<Vertex>, Vec<u16>) {
             add_vert(x, y + vert);
             add_vert(x, y - vert);
         }
+
         cycle = !cycle;
+
     }
 
     let indices = (0..vertices.len() as u16).collect();
